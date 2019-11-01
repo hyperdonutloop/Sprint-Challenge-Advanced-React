d02a404c4e8f9d6ce74e4f79c2edda6550cb0fa3
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-01 09:39:34 -0700

    added in two tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import PlayerData from './Components/PlayerData';
+import expectExport from 'expect';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+it('renders an element with text "womens world cup" text', () => {
+  const wrapper = rtl.render(
+  <PlayerData />
+  );
+  expect(wrapper.getByText(/women's world cup/i));
+});
+
+describe('player data module', () => {
+  describe('player information', () => {
+    it('has a player with the name "Alex Morgan"', () => {
+      const expectedOutput = "Alex Morgan";
+      const actualOutput = "Alex Morgan";
+      expect(actualOutput).toBe(expectedOutput);
+    })
+  })
+})
